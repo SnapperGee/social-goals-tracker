09aa5afef0b6b63f022af44965c5126991e0ea93
Snap <XxSnapperGeexX@gmail.com>
2023-11-14 15:53:38 -0800

    Init index.mjs.

---
+++ scripts/seed/index.mjs
@@ -0,0 +1,9 @@
+import { seedUsers } from './users.mjs';
+import { seedFriends } from './friends.mjs';
+import { seedGoals } from './goals.mjs';
+import { seedMilestones } from './milestones.mjs';
+
+await seedUsers();
+await seedFriends();
+await seedGoals();
+await seedMilestones();
