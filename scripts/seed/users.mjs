914b339688fa9b6757fbf56de89b7194978f3f30
Snap <XxSnapperGeexX@gmail.com>
2023-11-14 13:51:00 -0800

    Init scripts/seed/users.mjs.

---
+++ scripts/seed/users.mjs
@@ -0,0 +1,44 @@
+import { randomUUID } from "node:crypto";
+
+export default Object.freeze([
+    Object.freeze({
+        id: randomUUID(),
+        name: "Tim Henson",
+        password: "polyphia"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Keanu Reeves",
+        password: "thematrix"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Simba",
+        password: "thelionking"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Roger Smith",
+        password: "americandad"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "John Wick",
+        password: "bad#ss"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Kion",
+        password: "thelionguard"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Porter Robinson",
+        password: "potaro"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Zim",
+        password: "invader"
+    })
+]);
