b7ab71daeb7183b45df70a622e847e7f01ed69c2
Snap <XxSnapperGeexX@gmail.com>
2023-11-14 16:18:38 -0800

    Rename getRandomClient function to getRandomUser.

--- scripts/seed/friends.mjs
+++ scripts/seed/friends.mjs
@@ -1,14 +1,14 @@
 import { users } from "./users.mjs";
 import { PrismaClient } from "../../src/main/prisma/index.js";
 
-const getRandomClient = (indexToExclude) => users.filter((_, index) => index !== indexToExclude)[Math.floor(Math.random() * users.length - 1)];
+const getRandomUser = (indexToExclude) => users.filter((_, index) => index !== indexToExclude)[Math.floor(Math.random() * users.length - 1)];
 
 const prismaClient = new PrismaClient();
 
 export const friends = Object.freeze(users.filter((_, index) => index % 3 === 0).map((user, index) =>
     Object.freeze({
         source: user.id,
-        target: getRandomClient(index).id
+        target: getRandomUser(index).id
     })
 ));
 
