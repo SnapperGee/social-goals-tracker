4143c9ddf4cf6ea58041c954624ff973512ac383
Snap <XxSnapperGeexX@gmail.com>
2023-11-14 16:00:04 -0800

    Add more users to seed.

--- scripts/seed/users.mjs
+++ scripts/seed/users.mjs
@@ -26,6 +26,11 @@ export const users = Object.freeze([
     }),
     Object.freeze({
         id: randomUUID(),
+        name: "Bjarne Stroustrup",
+        password: "cplusplus"
+    }),
+    Object.freeze({
+        id: randomUUID(),
         name: "John Wick",
         password: "bad#ss"
     }),
@@ -36,13 +41,53 @@ export const users = Object.freeze([
     }),
     Object.freeze({
         id: randomUUID(),
+        name: "Steve Jobs",
+        password: "apple"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Linus Torvalds",
+        password: "linux"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Bill Gates",
+        password: "microsoft"
+    }),
+    Object.freeze({
+        id: randomUUID(),
         name: "Porter Robinson",
         password: "potaro"
     }),
     Object.freeze({
         id: randomUUID(),
+        name: "Ron Swanson",
+        password: "parks&rec"
+    }),
+    Object.freeze({
+        id: randomUUID(),
         name: "Zim",
         password: "invader"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Button Mash",
+        password: "pony"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Tony Stark",
+        password: "ironman"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Ron Burgundy",
+        password: "anchorman"
+    }),
+    Object.freeze({
+        id: randomUUID(),
+        name: "Link",
+        password: "zelda"
     })
 ]);
 
